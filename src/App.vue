3b0a0dcdfd7e2e78845f39dae5548ea556686e1f
onitwin <onitwin@gmail.com>
2020-02-23 20:09:22 +0000

    mvp achieved

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
-    <filter-controls/>
+    <h1 id="greeting">Itsa Me, Mario!!</h1>
+    <filter-controls id="filt"/>
+    <img  id="reset" src="./assets/questionBlock.png" v-on:click="reset">
+    <audio id="myAudio">
+      <source src="./assets/smw_coin.wav" type="audio/ogg">
+  </audio>
+
     <list-all :allAmiibo="filteredAmiibo"></list-all>
   </div>
 </template>
@@ -9,7 +15,7 @@
 import ListAll from './components/ListAll.vue';
 import FilterControls from './components/FilterControls.vue'
 import {eventBus} from './main.js';
-
+import Block from './components/Block'
 export default {
   name: 'App',
   data(){
@@ -17,7 +23,9 @@ export default {
       allAmiibo:[],
       chosenAmiibo:null,
       selectedGame:null,
-      filteredAmiibo:[]
+      filteredAmiibo:[],
+      image:"./assets/questionBlock.png",
+      sound:"./assets/smw_coin.wav"
     }
 
   },
@@ -35,13 +43,24 @@ export default {
   },
   components:{
     "list-all":ListAll,
-    "filter-controls": FilterControls
+    "filter-controls": FilterControls,
+    "block":Block
 
   },
   computed:{
     filters:function(){
       return this.filteredAmiibo = this.filteredAmiibo.filter(amiibo => amiibo.gameSeries === this.selectedGame.name)}
 
+
+  },
+  methods:{
+     reset:function(){
+       var myBlock = document.getElementById("myAudio");
+       myBlock.play()
+       this.selectedGame=null
+       this.filteredAmiibo=this.allAmiibo;
+
+    }
 
   }
 
@@ -51,4 +70,26 @@ export default {
 </script>
 
 <style>
+
+
+@@font-face {
+  font-family:"Mario";
+  src:url(./assets/SuperMario256.ttf);
+}
+
+
+#reset{
+  height:100px;
+  width:100px;
+  display: inline-flex;
+  margin-left: 400px;
+}
+
+#reset:active{
+  transform: translatey(4px);
+}
+
+#filt{
+  display: inline-flex;
+}
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <div id="app">
+    <img id="block" :src="./assets/questionBlock">
+  </div>
+</template>
+
+<script>
+export default {
+  name:"block",
+  props:["image"]
+}
+</script>
+
+<style lang="css" scoped>
+</style>
+
+
+<!-- this component is not used but would like to discuss why i couldnt get it to work -->
